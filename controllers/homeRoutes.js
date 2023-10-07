f28859941ba1feca12a904c7be21c80330705bca
unknown <douggouthro@gmail.com>
2023-10-06 21:36:47 -0500

    added routes for login and signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,5 +7,17 @@ router.get('/', async (req, res) => {
  
 });
 
+router.get('/signup', async (req, res) => {
+ 
+  res.render('signup');
+
+});
+
+router.get('/login', async (req, res) => {
+ 
+  res.render('login');
+
+});
+
 
 module.exports = router;
